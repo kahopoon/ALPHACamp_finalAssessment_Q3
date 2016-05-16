2bab71879e8dfd15c1ff01aa4c394c4dae0730ec
Johnson Kaho Poon <kahopoon@gmail.com>
2016-05-16 14:22:47 +0800

    bello bello~~

--- ALPHACamp_finalAssessment_Q3/ViewController.swift
+++ ALPHACamp_finalAssessment_Q3/ViewController.swift
@@ -12,14 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("\(#function)")
     }
 
+    override func viewDidAppear(animated: Bool) {
+        print("\(#function)")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        print("\(#function)")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        print("\(#function)")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        print("\(#function)")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
